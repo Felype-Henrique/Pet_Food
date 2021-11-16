b3c06478d04f1976785c7f0e0013f50638cfffe0
Felype Henrique <felypehenrique25@gmail.com>
2021-11-15 21:20:29 -0300

    Iniciando primeira página

--- web/src/pages/cadastro/index.js
+++ web/src/pages/cadastro/index.js
@@ -1,5 +1,15 @@
+import Logo from '../../assets/logo-white.png'
+
 const Cadastro = () =>{
-    return <h1>Cadastro</h1>
+    return (
+        <div className="container-fluid h-100 bg-primary">
+            <div className="col-12">
+                <img src={Logo}/>
+            </div>
+            <div className="col-6"></div>
+            <div className="col-6"></div>
+        </div>
+    )
 }
 
 export default Cadastro;
