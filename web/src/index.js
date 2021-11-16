e9bf184419a96417f3de25a63ec954623d270144
Felype Henrique <felypehenrique25@gmail.com>
2021-11-16 09:46:41 -0300

    Página de checkout

--- web/src/index.js
+++ web/src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import './styles/global.css'
 
 import Cadastro from './pages/cadastro'
+import Checkout from './pages/checkout'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cadastro />
+    <Checkout />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ web/src/pages/checkout/index.js
@@ -0,0 +1,21 @@
+import Header from "../../components/header";
+
+const Checkout = () => {
+  return (
+    <div className="h-100">
+      <Header />
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-6">
+              <span>Dados de Entrega</span>
+              <div className="row mb-3">
+
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
