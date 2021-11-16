3c3a97a14baba1c70592786f8ae49952112b115d
Felype Henrique <felypehenrique25@gmail.com>
2021-11-16 09:37:01 -0300

    Finalizando pagina de cadastro

--- web/src/pages/cadastro/index.js
+++ web/src/pages/cadastro/index.js
@@ -1,15 +1,55 @@
-import Logo from '../../assets/logo-white.png'
+import Header from "../../components/header";
+import Illustration from "../../assets/illustration.png";
 
-const Cadastro = () =>{
-    return (
-        <div className="container-fluid h-100 bg-primary">
-            <div className="col-12">
-                <img src={Logo}/>
-            </div>
-            <div className="col-6"></div>
-            <div className="col-6"></div>
+const Cadastro = () => {
+  return (
+    <div className="container-fluid h-100 bg-primary">
+      <Header whiteVersion/>
+      <div className="row">
+        <div className="col-6 text-right my-auto">
+          <img src={Illustration} className="img-fluid"/>
         </div>
-    )
-}
+        <div className="col-6">
+          <div className="box col-8">
+            <h2 className="text-center">Falta pouco pra fazer seu pet feliz.</h2>
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="Nome completo"
+            />
+
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="Email"
+            />
+
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="Telefone"
+            />
+
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="CPF"
+            />
+
+            <input
+              type="date"
+              className="form-control form-control-lg mb-3"
+              placeholder="Data de nascimento"
+            />
+
+            <button className="btn btn-lg btn-block btn-secOndary">
+                Finalizar pedido
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Cadastro;
