84cab34a014624d67f7fc7bb82cdb39a8c2841b7
Felype Henrique <felypehenrique25@gmail.com>
2021-11-17 11:00:07 -0300

    Página petshop avaliação

---
+++ web/src/pages/petshop/index.js
@@ -0,0 +1,32 @@
+import Header from '../../components/header'
+import './styles.css'
+
+const Petshop = () => {
+    return(
+        <div className="h-100">
+            <Header/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-2">
+                        <img src="https://www.petlove.com.br/static/uploads/banner_image/image/4304/logo-petlove-push.png" className="img-fluid petshop-image"/>
+                        <b>PetLove</b>
+                        <div className="petshop-infos">
+                            <span className="mdi mdi-star"></span>
+                            <text>
+                                <b>2,8</b>
+                            </text>
+                            <span className="mdi mdi-cash"></span>
+                            <text>$$$</text>
+                            <span className="mdi mdi-crosshairs-gps"></span>
+                            <text>2,9 Km</text>
+                        </div>
+                        <label className="badge badge-primary">Frete Grátis</label>
+                    </div>
+                    <div className="col-10"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Petshop;
